faa3cd6b286a15a93a3366f9b068a58ed4eba3c8
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 18:19:10 +0530

    chore: Update npm dependencies
    - Add "@types/jsonwebtoken" dependency
    - Update "jsonwebtoken" dependency version
    - Remove unused express dependency

--- config/env.config.ts
+++ config/env.config.ts
@@ -2,9 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT:string= process.env.PORT || '3000';
-const DBURI:string= process.env.DBURI || 'mongodb://localhost:27017/studentdb';
-const RECAPTCHA_SECRET_KEY:string|undefined= process.env.RECAPTCHA_SECRET_KEY ;
-const EMAIL:string|undefined= process.env.EMAIL;
-const PASS:string|undefined= process.env.PASS;
+const DBURI:string= process.env.DBURI ;
+const ACCESS_TOKEN_SECRET:string|undefined= process.env.ACCESS_TOKEN_SECRET;    
 
-export {PORT,DBURI,RECAPTCHA_SECRET_KEY,EMAIL,PASS};
+export {PORT,DBURI, ACCESS_TOKEN_SECRET};
