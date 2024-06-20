04f2505278db95aac0610196132c3a37ad22eb60
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 18:22:00 +0530

    chore: Update DBURI default value in env.config.ts

--- config/env.config.ts
+++ config/env.config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT:string= process.env.PORT || '3000';
-const DBURI:string= process.env.DBURI ;
+const DBURI:string= process.env.DBURI || "mongodb://localhost:27017/studentDb" ;
 const ACCESS_TOKEN_SECRET:string|undefined= process.env.ACCESS_TOKEN_SECRET;    
 
 export {PORT,DBURI, ACCESS_TOKEN_SECRET};
