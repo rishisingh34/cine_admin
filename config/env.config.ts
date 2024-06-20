216b422014b06bcb29b4c9fab4751450b87d68e9
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 19:59:54 +0530

    chore: Add EMAIL and PASS variables to env.config.ts

--- config/env.config.ts
+++ config/env.config.ts
@@ -4,5 +4,8 @@ dotenv.config();
 const PORT:string= process.env.PORT || '3000';
 const DBURI:string= process.env.DBURI || "mongodb://localhost:27017/studentDb" ;
 const ACCESS_TOKEN_SECRET:string|undefined= process.env.ACCESS_TOKEN_SECRET;    
+const EMAIL:string | undefined=process.env.EMAIL; 
+const PASS:string |undefined = process.env.PASS;
 
-export {PORT,DBURI, ACCESS_TOKEN_SECRET};
+
+export {PORT,DBURI, ACCESS_TOKEN_SECRET, EMAIL, PASS};
