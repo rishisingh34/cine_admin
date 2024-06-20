443fa183076e1bab3f7708462133ef41df272d69
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 20:04:45 +0530

    chore: Add endpoint to add student with authentication middleware

--- routes/admin.routes.ts
+++ routes/admin.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import adminController from "../controllers/admin.controllers";
+import auth from "../middleware/auth.middleware";   
 const router = Router();
 
 router.post('login', adminController.login); 
+router.post('addStudent',auth, adminController.addStudent);
 
 
 export default router;
