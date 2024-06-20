2ef24bfac7bb86875d8975a5efdb175f75e2172e
rishisingh34 <singh34rishi@gmail.com>
2024-06-20 20:02:32 +0530

    chore: Refactor admin controller to separate file

--- controllers/admin.controllers.ts
+++ controllers/admin.controllers.ts
@@ -1,9 +1,12 @@
+import Admin from '../models/admin.model';
 import { Request, Response } from 'express';
+import crypto from "crypto";
 import Question from '../models/question.model';
+import StudentModel from "../models/student.model";
 import ResponseModel from '../models/response.model';
 import Visited from '../models/visited.model';
 import Token from '../middleware/token.middleware';
-import Admin from '../models/admin.model';
+import {sendPassword} from "../utils/mailer";
 
 const adminController = {
     login: async (req: Request, res: Response): Promise<Response> => {
@@ -26,6 +29,32 @@ const adminController = {
         } catch (err) {
             console.error(err);
             return res.status(500).json({ message: "Internal Server Error" });
+        }
+    },
+    addStudent: async(req:Request,res:Response):Promise<Response>=>{
+        try {
+            const {name,studentNumber,branch,gender,residency,email,phone} = req.body;
+            const studentExists = await StudentModel.findOne({studentNumber});
+            if(studentExists){
+                return res.status(400).json({message:"Student already exists."});
+            }
+            const password:string= crypto.randomBytes(8).toString('hex');
+            const student = new StudentModel({
+                name,
+                studentNumber,
+                branch,
+                gender,
+                residency,
+                email,
+                phone,
+                password,
+                isVerified:true
+            });
+            await student.save();
+            await sendPassword(email,studentNumber,password);
+            return res.status(201).json({message:"Student registered successfully."});
+        } catch (error) {
+            return res.status(500).json({message:"Internal server error."});
         }
     }
 }
